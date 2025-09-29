baa1e487b700c721f71c6f4686178f86f751ac85
EvanChan67 <1539326@learn.vsb.bc.ca>
2025-09-29 21:19:29 +0000

    chanes

--- 2.4/main.py
+++ 2.4/main.py
@@ -1,5 +1,6 @@
 responses = open("2.4/responses.csv")
 for response in responses:
     response = response.split(",")
-    if response[1] == "Evan Chan":      
+    if response[1].lower() == "evan chan":      
         print(response)
+        me = response
